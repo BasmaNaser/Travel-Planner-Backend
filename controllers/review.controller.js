cc474c713de10c1ed8b125bd97238cfe8dd06140
Aseel Toson <aseeltoson423@gmail.com>
2026-09-25 23:47:51 +0300

    Update review controller and routes

--- controllers/review.controller.js
+++ controllers/review.controller.js
@@ -1,4 +1,226 @@
 
+// const Review = require("../models/review.model");
+
+// // =====================================
+// // Create Review
+// // LOGIN REQUIRED
+// // =====================================
+// const createReview = async (req, res, next) => {
+//   try {
+//     const {
+//       Rating,
+//       Comment,
+//       Platform,
+//       DestinationID,
+//     } = req.body;
+
+//     const review = await Review.create({
+//       Rating,
+//       Comment,
+//       Platform,
+//       DestinationID,
+//       UserID: req.user.id,
+//     });
+
+//     res.status(201).json({
+//       message: "Review created successfully",
+//       review,
+//     });
+//   } catch (error) {
+//     next(error);
+//   }
+// };
+
+// // =====================================
+// // Get Reviews By Destination
+// // PUBLIC - LOGIN NOT REQUIRED
+// // =====================================
+// const getReviewsByDestination = async (
+//   req,
+//   res,
+//   next
+// ) => {
+//   try {
+//     const reviews = await Review.find({
+//       DestinationID: req.params.destinationId,
+//       isDeleted: false,
+//     })
+//       .populate("UserID", "fullName email")
+//       .sort({ createdAt: 1 });
+
+//     res.status(200).json({
+//       message: "Reviews fetched successfully",
+//       reviews,
+//     });
+//   } catch (error) {
+//     next(error);
+//   }
+// };
+
+// // =====================================
+// // Update My Review
+// // LOGIN REQUIRED
+// // =====================================
+// const updateReview = async (req, res, next) => {
+//   try {
+//     const {
+//       Rating,
+//       Comment,
+//       Platform,
+//     } = req.body;
+
+//     const review = await Review.findOneAndUpdate(
+//       {
+//         _id: req.params.id,
+//         UserID: req.user.id,
+//         isDeleted: false,
+//       },
+//       {
+//         Rating,
+//         Comment,
+//         Platform,
+//       },
+//       {
+//         new: true,
+//         runValidators: true,
+//       }
+//     );
+
+//     if (!review) {
+//       return res.status(404).json({
+//         message:
+//           "Review not found or you are not allowed to update it",
+//       });
+//     }
+
+//     res.status(200).json({
+//       message: "Review updated successfully",
+//       review,
+//     });
+//   } catch (error) {
+//     next(error);
+//   }
+// };
+
+// // =====================================
+// // Delete Review
+// // LOGIN REQUIRED
+// // =====================================
+// const deleteReview = async (req, res, next) => {
+//   try {
+//     const review = await Review.findById(
+//       req.params.id
+//     );
+
+//     if (!review) {
+//       return res.status(404).json({
+//         message: "Review not found",
+//       });
+//     }
+
+//     if (review.isDeleted) {
+//       return res.status(400).json({
+//         message: "Review has already been deleted",
+//       });
+//     }
+
+//     // =================================
+//     // ADMIN DELETE
+//     // =================================
+//     if (req.user.role === "admin") {
+//       review.isDeleted = true;
+
+//       review.deletedReason =
+//         "Your review was removed by an administrator because it did not meet our community guidelines.";
+
+//       review.deletedAt = new Date();
+
+//       review.deletedNoticeRead = false;
+
+//       await review.save();
+
+//       return res.status(200).json({
+//         message:
+//           "Review deleted by admin successfully",
+//         review,
+//       });
+//     }
+
+//     // =================================
+//     // USER DELETE
+//     // =================================
+//     if (
+//       review.UserID.toString() !==
+//       req.user.id.toString()
+//     ) {
+//       return res.status(403).json({
+//         message:
+//           "You are not allowed to delete this review",
+//       });
+//     }
+
+//     await review.deleteOne();
+
+//     res.status(200).json({
+//       message: "Review deleted successfully",
+//     });
+//   } catch (error) {
+//     next(error);
+//   }
+// };
+
+// // =====================================
+// // Mark Deleted Review Notice As Read
+// // LOGIN REQUIRED
+// // =====================================
+// const markDeletedReviewNoticeRead = async (
+//   req,
+//   res,
+//   next
+// ) => {
+//   try {
+//     const review = await Review.findOneAndUpdate(
+//       {
+//         _id: req.params.id,
+//         UserID: req.user.id,
+//         isDeleted: true,
+//         deletedNoticeRead: false,
+//       },
+//       {
+//         deletedNoticeRead: true,
+//       },
+//       {
+//         new: true,
+//       }
+//     );
+
+//     if (!review) {
+//       return res.status(404).json({
+//         message:
+//           "Deleted review notice not found",
+//       });
+//     }
+
+//     res.status(200).json({
+//       message:
+//         "Deleted review notice marked as read",
+//     });
+//   } catch (error) {
+//     next(error);
+//   }
+// };
+
+// module.exports = {
+//   createReview,
+//   getReviewsByDestination,
+//   updateReview,
+//   deleteReview,
+//   markDeletedReviewNoticeRead,
+// };
+
+
+
+
 const Review = require("../models/review.model");
 
 // =====================================
@@ -58,10 +280,55 @@ const getReviewsByDestination = async (
 };
 
 // =====================================
+// Get My Deleted Review Notification
+// LOGIN REQUIRED
+// =====================================
+const getMyDeletedReviewNotification = async (
+  req,
+  res,
+  next
+) => {
+  try {
+    const review = await Review.findOne({
+      UserID: req.user.id,
+      isDeleted: true,
+      deletedNoticeRead: false,
+    }).populate(
+      "DestinationID",
+      "Name"
+    );
+
+    if (!review) {
+      return res.status(200).json({
+        notification: null,
+      });
+    }
+
+    res.status(200).json({
+      notification: {
+        reviewId: review._id,
+        message:
+          review.deletedReason ||
+          "Your review was removed by an administrator.",
+        destination:
+          review.DestinationID?.Name || "",
+      },
+    });
+
+  } catch (error) {
+    next(error);
+  }
+};
+
+// =====================================
 // Update My Review
 // LOGIN REQUIRED
 // =====================================
-const updateReview = async (req, res, next) => {
+const updateReview = async (
+  req,
+  res,
+  next
+) => {
   try {
     const {
       Rating,
@@ -69,22 +336,23 @@ const updateReview = async (req, res, next) => {
       Platform,
     } = req.body;
 
-    const review = await Review.findOneAndUpdate(
-      {
-        _id: req.params.id,
-        UserID: req.user.id,
-        isDeleted: false,
-      },
-      {
-        Rating,
-        Comment,
-        Platform,
-      },
-      {
-        new: true,
-        runValidators: true,
-      }
-    );
+    const review =
+      await Review.findOneAndUpdate(
+        {
+          _id: req.params.id,
+          UserID: req.user.id,
+          isDeleted: false,
+        },
+        {
+          Rating,
+          Comment,
+          Platform,
+        },
+        {
+          new: true,
+          runValidators: true,
+        }
+      );
 
     if (!review) {
       return res.status(404).json({
@@ -94,9 +362,11 @@ const updateReview = async (req, res, next) => {
     }
 
     res.status(200).json({
-      message: "Review updated successfully",
+      message:
+        "Review updated successfully",
       review,
     });
+
   } catch (error) {
     next(error);
   }
@@ -106,11 +376,17 @@ const updateReview = async (req, res, next) => {
 // Delete Review
 // LOGIN REQUIRED
 // =====================================
-const deleteReview = async (req, res, next) => {
+const deleteReview = async (
+  req,
+  res,
+  next
+) => {
   try {
-    const review = await Review.findById(
-      req.params.id
-    );
+
+    const review =
+      await Review.findById(
+        req.params.id
+      );
 
     if (!review) {
       return res.status(404).json({
@@ -120,28 +396,35 @@ const deleteReview = async (req, res, next) => {
 
     if (review.isDeleted) {
       return res.status(400).json({
-        message: "Review has already been deleted",
+        message:
+          "Review has already been deleted",
       });
     }
 
     // =================================
     // ADMIN DELETE
     // =================================
-    if (req.user.role === "admin") {
+    if (
+      req.user.role === "admin"
+    ) {
+
       review.isDeleted = true;
 
       review.deletedReason =
         "Your review was removed by an administrator because it did not meet our community guidelines.";
 
-      review.deletedAt = new Date();
+      review.deletedAt =
+        new Date();
 
-      review.deletedNoticeRead = false;
+      review.deletedNoticeRead =
+        false;
 
       await review.save();
 
       return res.status(200).json({
         message:
           "Review deleted by admin successfully",
+
         review,
       });
     }
@@ -149,10 +432,12 @@ const deleteReview = async (req, res, next) => {
     // =================================
     // USER DELETE
     // =================================
+
     if (
       review.UserID.toString() !==
       req.user.id.toString()
     ) {
+
       return res.status(403).json({
         message:
           "You are not allowed to delete this review",
@@ -162,8 +447,10 @@ const deleteReview = async (req, res, next) => {
     await review.deleteOne();
 
     res.status(200).json({
-      message: "Review deleted successfully",
+      message:
+        "Review deleted successfully",
     });
+
   } catch (error) {
     next(error);
   }
@@ -173,48 +460,65 @@ const deleteReview = async (req, res, next) => {
 // Mark Deleted Review Notice As Read
 // LOGIN REQUIRED
 // =====================================
-const markDeletedReviewNoticeRead = async (
-  req,
-  res,
-  next
-) => {
-  try {
-    const review = await Review.findOneAndUpdate(
-      {
-        _id: req.params.id,
-        UserID: req.user.id,
-        isDeleted: true,
-        deletedNoticeRead: false,
-      },
-      {
-        deletedNoticeRead: true,
-      },
-      {
-        new: true,
+const markDeletedReviewNoticeRead =
+  async (
+    req,
+    res,
+    next
+  ) => {
+
+    try {
+
+      const review =
+        await Review.findOneAndUpdate(
+          {
+            _id: req.params.id,
+            UserID: req.user.id,
+            isDeleted: true,
+            deletedNoticeRead: false,
+          },
+          {
+            deletedNoticeRead: true,
+          },
+          {
+            new: true,
+          }
+        );
+
+      if (!review) {
+
+        return res.status(404).json({
+          message:
+            "Deleted review notice not found",
+        });
+
       }
-    );
 
-    if (!review) {
-      return res.status(404).json({
+      res.status(200).json({
         message:
-          "Deleted review notice not found",
+          "Deleted review notice marked as read",
       });
+
+    } catch (error) {
+      next(error);
     }
+  };
 
-    res.status(200).json({
-      message:
-        "Deleted review notice marked as read",
-    });
-  } catch (error) {
-    next(error);
-  }
-};
-
+// =====================================
+// EXPORTS
+// =====================================
 module.exports = {
-  createReview,
-  getReviewsByDestination,
-  updateReview,
-  deleteReview,
-  markDeletedReviewNoticeRead,
-};
 
+  createReview,
+
+  getReviewsByDestination,
+
+  getMyDeletedReviewNotification,
+
+  updateReview,
+
+  deleteReview,
+
+  markDeletedReviewNoticeRead,
+
+};
--- routes/review.route.js
+++ routes/review.route.js
@@ -1,66 +1,170 @@
 
+// const express = require("express");
+
+// const router = express.Router();
+
+// const authentication = require("../middlewares/authMiddleware");
+
+// const {
+//   createReview,
+//   getReviewsByDestination,
+//   updateReview,
+//   deleteReview,
+//   markDeletedReviewNoticeRead,
+// } = require("../controllers/review.controller");
+
+// // =====================================
+// // Get Reviews By Destination
+// // PUBLIC - Login NOT Required
+// // =====================================
+// router.get(
+//   "/:destinationId",
+//   getReviewsByDestination
+// );
+
+// // =====================================
+// // Create Review
+// // LOGIN REQUIRED
+// // =====================================
+// router.post(
+//   "/",
+//   authentication,
+//   createReview
+// );
+
+// // =====================================
+// // Mark Deleted Review Notice As Read
+// // LOGIN REQUIRED
+// // =====================================
+// router.patch(
+//   "/notice/:id/read",
+//   authentication,
+//   markDeletedReviewNoticeRead
+// );
+
+// // =====================================
+// // Update My Review
+// // LOGIN REQUIRED
+// // =====================================
+// router.patch(
+//   "/:id",
+//   authentication,
+//   updateReview
+// );
+
+// // =====================================
+// // Delete Review
+// // LOGIN REQUIRED
+// // =====================================
+// router.delete(
+//   "/:id",
+//   authentication,
+//   deleteReview
+// );
+
+// module.exports = router;
+
+
+
+
+
+
+
+
+
 const express = require("express");
 
 const router = express.Router();
 
-const authentication = require("../middlewares/authMiddleware");
+const authentication =
+  require("../middlewares/authMiddleware");
 
 const {
+
   createReview,
+
   getReviewsByDestination,
+
+  getMyDeletedReviewNotification,
+
   updateReview,
+
   deleteReview,
+
   markDeletedReviewNoticeRead,
+
 } = require("../controllers/review.controller");
+
+
+// =====================================
+// Get My Deleted Review Notification
+// LOGIN REQUIRED
+// =====================================
+
+router.get(
+  "/notification/deleted",
+  authentication,
+  getMyDeletedReviewNotification
+);
+
 
 // =====================================
 // Get Reviews By Destination
-// PUBLIC - Login NOT Required
+// PUBLIC - LOGIN NOT REQUIRED
 // =====================================
+
 router.get(
   "/:destinationId",
   getReviewsByDestination
 );
 
+
 // =====================================
 // Create Review
 // LOGIN REQUIRED
 // =====================================
+
 router.post(
   "/",
   authentication,
   createReview
 );
 
+
 // =====================================
 // Mark Deleted Review Notice As Read
 // LOGIN REQUIRED
 // =====================================
+
 router.patch(
   "/notice/:id/read",
   authentication,
   markDeletedReviewNoticeRead
 );
 
+
 // =====================================
 // Update My Review
 // LOGIN REQUIRED
 // =====================================
+
 router.patch(
   "/:id",
   authentication,
   updateReview
 );
 
+
 // =====================================
 // Delete Review
 // LOGIN REQUIRED
 // =====================================
+
 router.delete(
   "/:id",
   authentication,
   deleteReview
 );
 
-module.exports = router;
 
+module.exports = router;
